81af39b47d555f9015c5aed7e8a97fd0f2111bdf
coookai <coookai@coookai.local>
2025-12-14 11:47:31 +0900

    Remove overview feature - simplify interface
    - Remove overview button from header
    - Delete member-overview.js file completely
    - Remove all overview-related CSS styles
    - Update app.js to remove overview initialization
    - Streamline interface to focus on core functionality

--- src/frontend/js/app.js
+++ src/frontend/js/app.js
@@ -116,15 +116,7 @@ class BandSyncCalendar {
             }
         });
         
-        // Overview button
-        const overviewButton = document.getElementById('overview-button');
-        if (overviewButton) {
-            overviewButton.addEventListener('click', () => {
-                if (typeof showMemberOverview !== 'undefined') {
-                    showMemberOverview();
-                }
-            });
-        }
+
         
         // Holiday button
         const holidayButton = document.getElementById('holiday-button');
